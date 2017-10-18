eaa23c26abc79c0c60ce1556febdb5d109554e34
mcasan <31909324+mcasan@users.noreply.github.com>
2017-10-18 09:39:53 -0400

    Add empty project

---
+++ Project_1(Part3)/Assets/Scripts/Medals_and_Cubes.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Medals_and_Cubes : MonoBehaviour {
+
+	int silver; 
+	int bronze;
+	int miningSpeed;
+	int TimetoMine;
+	int bronzeSupply;
+	int silverSupply;
+	int Xposition;
+	public GameObject cube;
+
+	// Use this for initialization
+	void Start () {
+
+	Xposition = 0;
+
+	miningSpeed = 3;
+
+	silver = 0;
+
+	bronze = 0;
+	
+	bronzeSupply = 3;
+
+	silverSupply = 2;
+
+	TimetoMine = miningSpeed;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Time.time >= TimetoMine) {
+
+			TimetoMine = TimetoMine + miningSpeed;
+
+
+			if (bronzeSupply > 0){
+
+				bronze = bronze + 1;
+
+				bronzeSupply = bronzeSupply - 1;
+
+				print (bronze + " bronze");
+
+				Xposition = Xposition + 2;
+
+				var mycube = Instantiate (cube, new Vector3 (Xposition,0), Quaternion.identity);
+
+				mycube.GetComponent<Renderer> ().material.color = Color.red;
+			 }
+
+			else if (bronzeSupply == 0){
+
+				if (silverSupply > 0) {
+					
+					
+					silver = silver + 1;
+
+					silverSupply = silverSupply -1 ;
+
+					print (silver + "silver");
+
+					Xposition = Xposition +2 ; 
+
+					var mycube = Instantiate (cube, new Vector3 (Xposition, 0), Quaternion.identity);
+					mycube.GetComponent<Renderer> ().material.color = Color.white;
+									  }
+				}
+
+		    }
+
+		
+				   }
+
+}
